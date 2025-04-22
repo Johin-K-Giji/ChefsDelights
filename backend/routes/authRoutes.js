23725d5fc525e1568b46dc8c675c9c598815eb7d
Johin123 <johinkgigi@gmail.com>
2025-04-22 10:28:42 +0530

    updated the alternte option

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const authController = require("./authController");
+const authController = require("../controllers/authController");
 
 router.post("/login", authController.login);
 
