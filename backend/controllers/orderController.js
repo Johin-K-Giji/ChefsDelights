ac3d1f5c32b9fd00eb0b3dc09602380806fa0aa3
Johin123 <johinkgigi@gmail.com>
2025-04-22 11:26:29 +0530

    updated order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,14 +1,26 @@
-const Order= require("../models/Order")
+const Order = require("../models/Order");
+const Product = require("../models/Product");
 
+const getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find();
 
-const getOrders = async(req,res)=>{
-    try{
-        const orders = await Order.find()
-        res.status(200).json(orders);
-    }catch(error){
-        res.status(500).json({ message: error.message });
-    }
+    // Enrich each order with product details
+    const ordersWithProductDetails = await Promise.all(
+      orders.map(async (order) => {
+        const products = await Product.find({ _id: { $in: order.productIds } });
+        return {
+          ...order.toObject(), // convert Mongoose doc to plain object
+          products, // attach full product info
+        };
+      })
+    );
 
-}
+    res.status(200).json(ordersWithProductDetails);
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
-module.exports={getOrders}
+module.exports = { getOrders };
